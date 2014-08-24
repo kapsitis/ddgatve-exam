94b1ba979ac12a861f3e6e96f42c5c53ba2b81aa
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2014-08-25 00:15:12 +0300

    Added rudimentary login

---
+++ app/controllers/Login.scala
@@ -0,0 +1,34 @@
+package controllers
+
+import play.api.mvc.{ Action, Controller }
+import models.Solution
+import java.math.BigInteger
+import java.security.SecureRandom
+import java.io.File
+import com.google.common.reflect.ClassPath.Scanner
+import java.util.Scanner
+
+object Login extends Controller {
+  val APPLICATION_NAME = "ddgatve-math"
+  val CLIENT_ID = "142624243903-vflqmb5336fmenqihfs41rokt42sbrt3.apps.googleusercontent.com"
+
+
+  def createStateToken = Action {
+    // Create a state token to prevent request forgery.
+    // Store it in the session for later validation.
+    implicit request =>
+      val state: String = new BigInteger(130, new SecureRandom()).toString(32)
+      request.session + ("state" -> state)
+      // Read index.html into memory, and set the client ID,
+      // token state, and application name in the HTML before serving it.
+//      val scanner = new Scanner(new File("index.html"), "UTF-8")
+//        .useDelimiter("\\A").next()
+//        .replaceAll("[{]{2}\\s*CLIENT_ID\\s*[}]{2}", CLIENT_ID)
+//        .replaceAll("[{]{2}\\s*STATE\\s*[}]{2}", state)
+//        .replaceAll("[{]{2}\\s*APPLICATION_NAME\\s*[}]{2}",
+//          APPLICATION_NAME);
+      val solutions = Solution.findAll
+      Ok(views.html.login.login("AAA", "BBB"))
+
+  }
+} 
